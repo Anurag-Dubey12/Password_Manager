8419ed9a44e5da5453cad06d8cea4c6c40e6e4b6
Anurag <dubeyjianurag@outlook.com>
2024-01-15 00:12:44 +0530

    initial

--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.passwordmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import me.ibrahimsn.lib.SmoothBottomBar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val bottom:SmoothBottomBar=findViewById(R.id.bottomBar)
+        var bottom:SmoothBottomBar=findViewById(R.id.bottomBar)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
