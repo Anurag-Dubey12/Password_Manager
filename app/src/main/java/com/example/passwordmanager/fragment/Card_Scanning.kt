75d72b46419f100f6a079c9eafab55c6af0787c4
Anurag <dubeyjianurag@outlook.com>
2024-02-25 12:10:20 +0530

    modified

--- app/src/main/java/com/example/passwordmanager/fragment/Card_Scanning.kt
+++ app/src/main/java/com/example/passwordmanager/fragment/Card_Scanning.kt
@@ -1,14 +1,10 @@
 package com.example.passwordmanager.fragment
 
-import ai.cardscan.insurance.CardScanActivity
 import ai.cardscan.insurance.CardScanActivityResult
 import ai.cardscan.insurance.CardScanActivityResultHandler
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Dialog
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,35 +13,10 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
-import androidx.annotation.OptIn
-import androidx.camera.core.CameraProvider
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ExperimentalGetImage
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.lifecycleScope
-import com.example.passwordmanager.Data_Class.CardDetails
-import com.example.passwordmanager.ExtractDataUseCase
 import com.example.passwordmanager.R
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlinx.coroutines.withContext
-import takePicture
-import java.util.concurrent.Executor
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
 
 class Card_Scanning : DialogFragment(), CardScanActivityResultHandler {
     private val CAMERA_PERMISSION = 7762
@@ -56,8 +27,6 @@ class Card_Scanning : DialogFragment(), CardScanActivityResultHandler {
     private lateinit var ownerTextView: TextView
     private lateinit var numberTextView: TextView
     private lateinit var dateTextView: TextView
-    private val useCase = ExtractDataUseCase(TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS))
-
     val TAG = "Card_Scanning"
 
     fun display(fragmentManager: FragmentManager): Card_Scanning {
